d6d94b6bb7bd8235c303dbe960ba7b5cd3a87088
avinash.jh <avvinashjha@gmail.com>
2026-06-15 06:52:29 +0530

    module lock added

--- public/js/course-progress.js
+++ public/js/course-progress.js
@@ -360,7 +360,8 @@ var DCCourseProgress = (function () {
   // (Previously: module 0 unlocked, later modules unlocked once the prior one
   // was fully complete.)
   function moduleUnlocked(_structure, _row, _moduleIndex) {
-    return true;
+    if (_moduleIndex <= 0) return true;
+    return moduleComplete(_structure, _row, _moduleIndex - 1);
   }
 
   function courseComplete(structure, row) {
